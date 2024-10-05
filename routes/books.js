1163f273372f8de5df7548faf8bf799e6cc94e72
sodabarauf <sodabarauf4@gmail.com>
2024-10-05 22:03:54 +0430

    solved linters error

--- routes/books.js
+++ routes/books.js
@@ -1,25 +1,25 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
 let books = [
-  { id: 1, title: "Book One", author: "Author One", description: "Description One", price: 10.99, image: "images/book1.jpg" },
-  { id: 2, title: "Book Two", author: "Author Two", description: "Description Two", price: 12.99, image: "images/book2.jpg" },
+  { id: 1, title: 'Book One', author: 'Author One', description: 'Description One', price: 10.99, image: 'images/book1.jpg' },
+  { id: 2, title: 'Book Two', author: 'Author Two', description: 'Description Two', price: 12.99, image: 'images/book2.jpg' },
 ];
 
 // GET all books
-router.get("/", (req, res) => {
+router.get('/', (req, res) => {
   res.json(books);
 });
 
 // POST new book
-router.post("/", (req, res) => {
+router.post('/', (req, res) => {
   const newBook = { id: books.length + 1, ...req.body };
   books.push(newBook);
   res.status(201).json(newBook);
 });
 
 // PUT update book
-router.put("/:id", (req, res) => {
+router.put('/:id', (req, res) => {
   const bookId = parseInt(req.params.id);
   const updatedBook = req.body;
   books = books.map((book) => (book.id === bookId ? { ...book, ...updatedBook } : book));
@@ -27,10 +27,10 @@ router.put("/:id", (req, res) => {
 });
 
 // DELETE book
-router.delete("/:id", (req, res) => {
+router.delete('/:id', (req, res) => {
   const bookId = parseInt(req.params.id);
   books = books.filter((book) => book.id !== bookId);
-  res.json({ message: "Book deleted" });
+  res.json({ message: 'Book deleted' });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,26 +1,24 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const path = require("path");
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 const PORT = 3000;
-
-const booksRoute = require("./routes/books");
-app.use("/books", booksRoute);
+const booksRoute = require('./routes/books');
+app.use('/books', booksRoute);
 
 // Middleware to handle JSON data
 app.use(bodyParser.json());
 
 // Serve static files from the 'public' directory
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 // Basic route for the homepage
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
-
